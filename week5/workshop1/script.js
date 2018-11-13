584e49ec1f60d7d5a585e8668737693cf631fb65
Tooyosi <oluwatoyosi.oyegoke@gmail.com>
2018-11-13 20:06:35 +0100

    add script.js functions

--- week5/workshop1/script.js
+++ week5/workshop1/script.js
@@ -1,29 +1,16 @@
-// (function(){
-//   console.log("This works");
-// }())
-// Function Declaration
-function hello() {
-  console.log("Hello everyone");
-}
-hello();
+$(document).ready(()=>{
+  
+  
+  var modalTitle = $(".modal-title");
+  var modalBody = $(".modal-body");
+  var modalFooter = $(".modal-footer");
+  $('button[type = "button"]').click(()=>{
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
 
-// Named Function Expression
-var howdy = function() {
-  console.log("Howdy everyone");
-};
-howdy();
-
-// Anonymous Function Expression
-// Function runs after 1 second
-setTimeout(function() {
-  console.log("Hey there");
-}, 1000);
-
-// IIFE
-// (new Date()).getFullYear()
-(function(){
-  console.log("IIFE 1")
-})();
-(function(){
-  console.log("IIFE 2")
-}());
+    modalTitle.text("Hello " + name);
+    modalBody.text("We recieved a message: " +message);
+    modalFooter.text("A reply has been sent to " + email);
+  })
+})
