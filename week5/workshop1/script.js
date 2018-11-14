f5fa7250715220f2b1c6bb5e2ec3a3f9cf37b496
Tooyosi <oluwatoyosi.oyegoke@gmail.com>
2018-11-14 09:32:11 +0100

    update script.js

--- week5/workshop1/script.js
+++ week5/workshop1/script.js
@@ -4,13 +4,19 @@ $(document).ready(()=>{
   var modalTitle = $(".modal-title");
   var modalBody = $(".modal-body");
   var modalFooter = $(".modal-footer");
-  $('button[type = "button"]').click(()=>{
+  $('button[type = "submit"]').click((e)=>{
     var name = $("#name").val();
     var email = $("#email").val();
     var message = $("#message").val();
 
-    modalTitle.text("Hello " + name);
-    modalBody.text("We recieved a message: " +message);
-    modalFooter.text("A reply has been sent to " + email);
-  })
+    if(name !='' || email !='' || message !=''){
+      modalTitle.text("Hello " + name);
+      modalBody.text("We recieved a message: " +message);
+      modalFooter.text("A reply has been sent to " + email); 
+    }else{
+      modalTitle.text("Please input your name");
+      modalBody.text("Please input your message");
+      modalFooter.text("Please input your email" ); 
+    }
+    })
 })
